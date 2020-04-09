60357bf8bcf96d427a4f036179b2ff1f7109a26d
CSharma610 <62789746+CSharma610@users.noreply.github.com>
2020-04-09 20:54:04 +0530

    Add files via upload

---
+++ project.11.c
@@ -0,0 +1,138 @@
+/*Question number 11.
+//Chandan Sharma(11812766)
+//CSE_316 Assignment
+Suppose that a disk has 5,000 cylinders, numbered 0 to 4999. The drive is currently serving a request at cylinder 143,
+and the previous request at cylinder 125. The queue of the pending requests, in FIFO. */
+
+//start
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+//Declatation and Initialization
+	int i=0,j=0,p=0,k=0,x=0,num,upr_limit,distance=0,cur_request,pre_request,r1_l0,dummy,q;
+	int que[40],right[40],left[40],final[40];
+	
+//Input
+	printf("\nINPUT-->");
+	printf("\n\tENTER THE NUMBER OF CYLINDERS CONTAINED BY DISK DRIVE : \n\t--> ");
+	scanf("\t%d",&upr_limit);
+	printf("\tENTER THE CURRENT REQUEST(POSITION) : \n\t--> ");
+	scanf("\t%d",&cur_request);
+	printf("\tENTER THE PREVIOUS REQUEST(POSITION) : \n\t--> ");
+	scanf("\t%d",&pre_request);
+	printf("\tENTER THE NUMBER OF ELEMENTS IN THE QUEUE OF PENDING REQUEST, IN FIFO(HOW MANY) : \n\t--> ");
+	scanf("\t%d",&num);
+	
+//Entering the pending request in queue to process
+	printf("NOW ENTER THE REQUEST OF THE PENDING QUEUE ONE BY ONE AND TAP ENTER AFTER EACH ENTRY***\n");
+	printf("REQUEST-->\n");
+	for(i=0;i<num;i++)
+	{
+		scanf("\t%d",&que[i]);
+	}
+	
+//Checking and seperating the request lesser than the current request into a another queue(array)
+    for(i=0;i<num;i++)
+    {
+    	if(que[i]<cur_request)
+    	{
+    		left[j]=que[i];
+    		j++;
+		}
+//Checking and seperating the request greater than the current request into a another queue(array)
+		if(que[i]>cur_request)
+		{
+			right[k]=que[i];
+			k++;
+		}
+	}
+	
+//Arranging requests in left side lower queue in decending order for easy pic up
+	for(i=0;i<j;i++)
+	{
+		for(p=0;p<j-1;p++)
+		{
+			if(left[p]<left[p+1])
+			{
+				dummy=left[p];
+				left[p]=left[p+1];
+				left[p+1]=dummy;
+			}
+		}
+	}
+	
+
+//Arranging requests in right side upper queue in ascending order for easy pic up
+	for(i=0;i<=k;i++)
+	{
+		for(p=0;p<k-1;p++)
+		{
+			if(right[p]>right[p+1])
+			{
+				dummy=right[p];
+				right[p]=right[p+1];
+				right[p+1]=dummy;
+			}
+		}
+	}
+	
+	
+//Checking for direction where to move on the basis of previous and current request/position
+if((pre_request-cur_request)<0)
+r1_l0=1;//r1 means for right ->1  
+else
+r1_l0=0;	//l0 mean for left ->0
+	switch(r1_l0)
+	{
+		case 0: //Start with left lower side of current position and move till end
+			for(i=0;i<j;i++)
+			{
+				final[x]=left[i];
+				x++;
+			}
+			final[x]=0;
+			x++;
+			for(i=0;i<k;i++) //Then move towars the right upper side till the max request in the queue
+			{
+				final[x]=right[i];
+				x++;
+			}
+			break;
+		case 1: //Start with right upper side of current position and move till end
+			for(i=0;i<k;i++)
+			{
+				final[x]=right[i];
+				x++;
+			}
+			final[x]=upr_limit-1;
+			x++;
+			for(i=0;i<j;i++)  //Then mover towards the left lower side till the min request in the queue.
+			{
+				final[x]=left[i];
+				x++;
+			}
+			break;
+	}
+	
+printf("\nOUTPUT-->");
+if(r1_l0==1)
+printf("\n\tMOVES TOWARS RIGHT SATISFYING ALL REQUEST TILL END OF CYLINDER AND THEN MOVES TOWARD LEFT TILL THE MIN REQUEST(MOVE --->> THEN <<---)\n ");
+if(r1_l0==0)
+printf("\n\tMOVES TOWARS LEFT SATISFYING ALL REQUEST TILL END 0 AND THE MOVES TOWARD RIGHT TILL THE MAX REQUEST(MOVE <--- THEN --->)\n");
+//Final queue with all the traversed request
+	printf("\n\n\tTHE FINAL QUEUE WITH SATISFYING ALL THE PENDING REQUEST IS : \n\n");
+	distance=distance+abs(cur_request-final[0]);  //Calculating diatance
+	printf("\t%d , %d",cur_request,final[0]);
+	for(i=1;i<x;i++)
+	    {
+	    	distance=distance+abs(final[i]-final[i-1]);
+	    	printf(" , %d",final[i]);
+	    }
+	 
+	printf("\n\nTOTAL DISTANCE(IN CYLINDER) THE DISK ARM MOVES : %d\n",distance);
+
+}
+//Done
+//End of program
+
---
+++ project.14.cpp
@@ -0,0 +1,221 @@
+/*Question no. 14
+//Chandan Sharma (11812766)
+//CSE_316 Assignment
+
+//If a teacher is being served at the food mess and during the period when he is being served,
+//another teacher comes, then that teacher would get the service(food) next.
+//This process might continue leading to increase in the waiting time of students to get food. 
+//Ensure in your program that the waiting time of student is minimized.
+*/
+
+
+//start
+#include <iostream>
+#include <bits/stdc++.h> 
+
+using namespace std; 
+
+int t_request;
+  
+
+struct person //to hold inputs
+{ 
+//initialization
+int arr_time,bru_time,priority,process_no; 
+}; 
+
+person serve[30]; 
+
+//if arival time same, comparing to sort as per priority
+bool comp(person a,person b) 
+{ 
+if(a.arr_time == b.arr_time) 
+{ 
+return a.priority<b.priority; 
+} 
+else
+{ 
+	return a.arr_time<b.arr_time; 
+} 
+} 
+
+//temp waiting time and temp turnaround time is for calculating starting time and complition time
+//find temp waiting time by FCFS 
+void get_temp1_time(int temp1[]) 
+{ 
+
+int cum_burst[30]; //cum_burst array to store cummatative burst time
+
+
+cum_burst[0]=0; 
+temp1[0]=0; 
+
+
+for(int i=1;i<t_request;i++) 
+{ 
+cum_burst[i]=serve[i-1].bru_time+cum_burst[i-1]; 
+
+temp1[i]=cum_burst[i]-serve[i].arr_time; 
+
+	
+	if(temp1[i]<0) 
+	{ 
+	temp1[i]=0; //if temp waiting chage to 0 if negative
+	} 
+} 
+
+} 
+
+void get_temp_time(int temp[],int temp1[]) 
+{ 
+
+
+for(int i=0;i<t_request;i++) //temp turnaround array
+{ 
+	temp[i]=temp1[i]+serve[i].bru_time; 
+} 
+	
+} 
+
+void calculation() 
+{ 
+
+int temp1[30],tarr_time[30],temp[30]; //declaring temp waiting time and turnaround around time
+
+double wait_avg=0,tarr_avg=0,student_wt=0; 
+
+//temp1=temp waiting time
+//temp=temp turnaround time
+//temp2=temprorary variable
+get_temp1_time(temp1); //function call for temp waiting time for starting and complition time calculation 
+ 
+get_temp_time(temp,temp1); //function call to find temp turnaround time
+
+int temp2;	
+int strt_time[30],comp_time[30]; 
+ 
+for(int i=1;i<t_request;i++) 
+	{ 
+	     if(serve[i].arr_time==0)
+	     {
+	     	strt_time[0]=0; 
+            comp_time[0]=strt_time[0]+temp[0];
+	        strt_time[i]=comp_time[i-1]; 
+		    comp_time[i]=strt_time[i]+temp[i]-temp1[i];	
+		 }
+		 else
+		 {
+		 	strt_time[0]=1; 
+            comp_time[0]=strt_time[0]+temp[0];
+	     	strt_time[i]=comp_time[i-1]; 
+	    	comp_time[i]=strt_time[i]+temp[i]-temp1[i];
+	     }
+
+	} 
+int wait[30];
+for(int i=0;i<t_request;i++)
+{
+		    
+	tarr_time[i]=comp_time[i]-serve[i].arr_time;   //filling turnaround array
+	
+	if(t_request==1)
+	{
+		 if(serve[i].arr_time==0)
+	     {
+	     	strt_time[0]=0; 
+            comp_time[0]=strt_time[0]+temp[0];
+	  }
+		 else
+		 {
+		 	strt_time[0]=1; 
+            comp_time[0]=strt_time[0]+temp[0];
+	     }
+		wait[i]=0;      //filling waiting array
+	}
+	else
+	{
+	wait[i]=tarr_time[i]-serve[i].bru_time;  //filling waiting array
+	}
+}
+	
+//displaying output
+cout<<"OUTPUT :-"<<endl;
+cout<<endl;
+cout<<"PROCESS_NUMBER      PRIORITY_QUEUE       START_TIME    COMPLITION_TIME      TURNARIUND_TIME       WAITING_TIME"<<endl; 
+	
+	
+for(int i=0;i<t_request;i++) 
+	{ 
+		wait_avg += wait[i]; 
+		tarr_avg += tarr_time[i]; 
+		
+		if(serve[i].priority<serve[i+1].priority)
+		{
+			temp2=serve[i].priority;
+			serve[i].priority=serve[i+1].priority;
+			serve[i+1].priority=temp2;
+		}
+		
+		if(serve[i].priority==1)
+		{
+			student_wt=student_wt+wait[i];
+		}
+		
+		cout<<"     "<<serve[i].process_no<<"\t\t\t   "<<serve[i].priority<<"    \t\t"<< 
+			strt_time[i]<<"\t\t  "<<comp_time[i]<<"      \t\t"<< 
+			tarr_time[i]<<"\t\t  "<<wait[i]<<endl; 
+	} 
+	
+	cout<<endl;
+	cout<<"\tOVERALL AVERAGE WAITING TIME IN THIS FOOD SERVING PROCESS : "; 
+	cout<<wait_avg/(float)t_request<<endl;   //average waiting time
+	cout<<"\tOVERALL AVERAGE TURNAROUND TIME IN THIS FOOD SERVING PROCESS : "; 
+	cout<<tarr_avg/(float)t_request<<endl;  //average turnaround time
+
+	cout<<"\tTHE OVERALL MINIMUM TIME TILL STUDENTS WAIT UNTIL THEY ALL BEING SERVED IS : ";
+	cout<<student_wt<<endl;    //student waiting time
+
+ 
+}
+int main()   //main method
+{ 
+int arrivaltime[30],bursttime[30],priority[30],p;
+
+//taking input
+cout<<"INPUT :-"<<endl;
+
+cout<<"ENTER TOTAL NUMBER OF PROCESS(TEACHER AS WELL AS STUDENT) :  ";
+cin>>t_request;
+cout<<endl;
+
+for(p=0;p<t_request;p++)
+{
+cout<<"ENTER THE ARRIVAL TIME FOR "<<p+1<<" :  ";
+cin>>arrivaltime[p];
+cout<<"ENTER THE BURST TIME FOR "<<p+1<<" :  ";
+cin>>bursttime[p]; 
+cout<<"ENTER THE PRIORITY OF PROCESS "<<p+1<<"(TEACHER=2 & STUDENT=1)"" :  ";
+cin>>priority[p];
+cout<<endl; 
+}
+for(int i=0;i<t_request;i++) 
+    { 
+    	serve[i].arr_time=arrivaltime[i]; 
+    	serve[i].bru_time=bursttime[i]; 
+    	serve[i].priority=priority[i]; 
+    	serve[i].process_no=i+1; 
+	}
+	  
+sort(serve,serve+t_request,comp);  //using inbuilt sort function
+	
+calculation(); //calling calculation function
+
+return 0; 
+
+} 
+
+//Done
+//End of program
+ 
+
+
